9982190c35469c6b748b387fd2eef0765d265d98
Раиса Чухнова <Raya.chuhnova@yandex.ru>
2025-11-08 12:02:57 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "1.9.0"
     application
     id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
-    id("dev.detekt") version "1.23.3"
+    id("dev.detekt") version("2.0.0-alpha.1")
 }
 
 group = "org.example"
