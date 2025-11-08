3f5129df63120fb68e2081053ec497dd9b0ec2ad
Раиса Чухнова <Raya.chuhnova@yandex.ru>
2025-11-08 10:50:30 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    testImplementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+    testImplementation("org.jetbrains.ktlin:ktlin-stdlib:1.9.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
     testImplementation(kotlin("test"))
     //testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
