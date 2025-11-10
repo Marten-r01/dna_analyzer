f361669ee348004f62a5cbcc6f82c36270314e1e
Cunitsa-r <Raya.chuhnova@yandex.ru>
2025-11-11 01:57:44 +0300

    fixed linter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,22 @@
+@file:Suppress("ktlint")
 plugins {
     kotlin("jvm") version "1.9.23" // Обновлено
     application
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1" // Обновлено
 }
-
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
-
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib") // Уточнено
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
-
 application {
     mainClass.set("org.example.MainKt")
 }
-
 tasks.test {
     useJUnitPlatform()
     testLogging {
